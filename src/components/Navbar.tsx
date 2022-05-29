c08b1f89c8dc9dccfe1acd7d82e64a6c784b411f
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-05-29 18:26:11 -0500

    Add: basic styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,19 @@ function Navbar() {
     <div className='Navbar'>
       <div className="Wrapper">
         <div className="Logo-Container">
-          <h2>Logo</h2>
+          <h2>React Navigation Test</h2>
         </div>
-        <div className="Links-Container"></div>
-        <div className="Account-Container"></div>
+        <div className="Links-Container">
+          <ul role="navigation">
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+        </div>
+        <div className="Account-Container">
+          <p>Last Updated: Today</p>
+        </div>
       </div>
     </div>
   )
