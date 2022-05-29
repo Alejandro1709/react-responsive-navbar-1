e5eb48f126798772392de7d863982767750459a5
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-05-29 18:08:01 -0500

    add navbar markup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='Navbar'>
+      <div className="Wrapper">
+        <div className="Logo-Container">
+          <h2>Logo</h2>
+        </div>
+        <div className="Links-Container"></div>
+        <div className="Account-Container"></div>
+      </div>
+    </div>
   )
 }
 
